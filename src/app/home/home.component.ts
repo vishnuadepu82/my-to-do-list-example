36cfa869c6bbbf1784a8ed170ac8b362a7656951
vishnuadepu82 <vishnu.adepu82@gmail.com>
2020-07-22 05:58:04 +0530

    3 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+itemCount:number;
+projectText:string="Angulat project";
+projects=[]
   constructor() { }
 
   ngOnInit() {
+    this.itemCount=this.projects.length;
   }
-
+additems(){
+  this.projects.push(this.projectText);
+  this.projectText="";
+  this.itemCount=this.projects.length;
+}
+removeItems(i)
+{
+this.projects.splice(i,1);
+this.itemCount=this.projects.length;
+}
 }
